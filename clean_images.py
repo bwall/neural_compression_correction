272c842ca5e89190a5b4d8c9e979f4163ae55a14
bwall <bwall9809@gmail.com>
2019-12-08 08:39:45 -0800

    Added super resolution flag

--- clean_images.py
+++ clean_images.py
@@ -2,7 +2,7 @@
 import argparse
 import torchvision.transforms as transforms
 import torch.utils.data
-from neural_compression_correction.models import load_dn4
+from neural_compression_correction.models import load_dn4, load_dn9, load_dn10_sharpen, load_dn13na, load_dn14na
 
 from tqdm import tqdm
 
@@ -19,29 +19,40 @@ def mirror_directory_structure(original_base, new_base):
     for root, dirs, _ in os.walk(original_base):
         for dr in dirs:
             p = os.path.join(root.replace(original_base, new_base), dr)
-            os.makedirs(p)
+            try:
+                os.makedirs(p)
+            except Exception as e:
+                print(e)
             
 
 def list_file_paths(base_path):
     paths = []
     for root, dirs, fns in os.walk(base_path):
+        if ".ipynb_checkpoints" in root:
+            continue
         for fn in fns:
-            paths.append(os.path.join(root, fn))
+            if fn.lower().split(".")[-1] in {"jpg", "jpeg", "png"}:
+                print(os.path.join(root, fn))
+                paths.append(os.path.join(root, fn))
     return paths
 
 
 class RawPathedImageDataset(torch.utils.data.Dataset):
-    def __init__(self, image_paths):
+    def __init__(self, image_paths, super_resolution=False):
         self.image_paths = list(image_paths)
         self.length = len(self.image_paths)
         self.to_tensor = transforms.ToTensor()
+        self.super_resolution = super_resolution
     
     def __len__(self):
         return self.length
 
     def __getitem__(self, idx):
         path, new_path = self.image_paths[idx]
-        oimage = self.to_tensor(Image.open(path).convert("RGB"))
+        img = Image.open(path).convert("RGB")
+        if self.super_resolution:
+            img = img.resize((img.size[0] * 2, img.size[1] * 2))
+        oimage = self.to_tensor(img)
         
         return (new_path, oimage)
 
@@ -57,10 +68,12 @@ def main():
                         help='disables CUDA training')
     parser.add_argument('-i', '--input', type=str, required=True, help="Path to file or directory of images to process")
     parser.add_argument('-o', '--output', type=str, required=True, help="Path to file or directory to output results to")
+    parser.add_argument('-s', '--super', action='store_true', default=False, help="Doubles image resolution")
 
     args = parser.parse_args()
     
     batch_size = args.batch_size
+    super_resolution = args.super
     single_file = os.path.isfile(args.input)
     if not single_file:
         paths = list_file_paths(args.input)
@@ -70,7 +83,7 @@ def main():
             (path, os.path.join(path.replace(args.input, output_path))) for path in paths
         ]
     else:
-        paths = [args.input, args.output]
+        paths = [(args.input, args.output)]
         batch_size = 1
     
     use_cuda = not args.no_cuda and torch.cuda.is_available()
@@ -78,7 +91,7 @@ def main():
     device = torch.device("cuda" if use_cuda else "cpu")
 
     loader = torch.utils.data.DataLoader(
-        RawPathedImageDataset(paths),
+        RawPathedImageDataset(paths, super_resolution=super_resolution),
         batch_size=batch_size, 
         shuffle=False,
         drop_last=False,
@@ -86,15 +99,29 @@ def main():
     )
 
     model = load_dn4(device=device)
+    #model = load_dn14na(device=device)
     unloader = transforms.ToPILImage()
     
     with torch.no_grad():
         
         for new_paths, image_batch in tqdm(loader):
-            preds = model(image_batch.to(device)).cpu()
-            for i, np in enumerate(new_paths):
-                img = unloader(preds[i, :, :, :])
-                img.save(np)
+            try:
+                preds = model(image_batch.to(device)).cpu()
+                for i, np in enumerate(new_paths):
+                    img = unloader(preds[i, :, :, :])
+                    img.save(np)
+                    del img
+                del preds
+                del image_batch
+            except RuntimeError as rte:
+                preds = model.cpu()(image_batch.to("cpu")).cpu()
+                for i, np in enumerate(new_paths):
+                    img = unloader(preds[i, :, :, :])
+                    img.save(np)
+                    del img
+                model.to(device)
+                del preds
+                del image_batch
         
 if __name__ == '__main__':
     main()
--- neural_compression_correction/models.py
+++ neural_compression_correction/models.py
@@ -53,6 +53,23 @@ class DecompressNet2(nn.Module):
         return F.hardtanh(self.decompress(x) + x, min_val=0.0, max_val=1.0)
     
 
+class Residual(nn.Module):
+    def __init__(self, in_features, kernel_size=3):
+        super(Residual, self).__init__()
+
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_features, in_features, kernel_size=kernel_size, padding=int(kernel_size / 2)),
+            nn.Dropout(0.1),
+            nn.ReLU(),
+            
+            nn.Conv2d(in_features, in_features, kernel_size=kernel_size, padding=int(kernel_size / 2)),
+            nn.Dropout(0.1),
+        )
+        
+    def forward(self, x):
+        return torch.tanh(x + self.conv(x))
+
+
 class ParallelResidual(nn.Module):
     def __init__(self, in_features):
         super(ParallelResidual, self).__init__()
@@ -115,9 +132,292 @@ class DecompressNet4(nn.Module):
     def forward(self, x):
         return F.hardtanh(self.decompress(x) + x, min_val=0.0, max_val=1.0)
     
+
+class DecompressNet9(nn.Module):
+    def __init__(self):
+        super(DecompressNet9, self).__init__()
+
+        self.decompress = nn.Sequential(
+            nn.Conv2d(3, 128, kernel_size=3, stride=1, padding=1),
+            nn.Dropout(0.1),
+            nn.ReLU(),
+            
+            Residual(128, 3),
+            nn.Dropout(0.1),
+            
+            Residual(128, 5),
+            nn.Dropout(0.1),
+            
+            Residual(128, 7),
+            nn.Dropout(0.1),
+            
+            Residual(128, 5),
+            nn.Dropout(0.1),
+            
+            Residual(128, 3),
+            
+            nn.Conv2d(128, 3, kernel_size=1, stride=1, padding=0),
+            nn.Hardtanh(min_val=-1.0, max_val=1.0),    
+        )
+        
+        self.attention = nn.Sequential(
+            nn.Conv2d(3, 128, kernel_size=3, stride=1, padding=1),
+            nn.Dropout(0.1),
+            nn.ReLU(),
+            
+            Residual(128, 3),
+            nn.Dropout(0.1),
+            
+            Residual(128, 5),
+            nn.Dropout(0.1),
+            
+            Residual(128, 7),
+            nn.Dropout(0.1),
+            
+            Residual(128, 5),
+            nn.Dropout(0.1),
+            
+            Residual(128, 3),
+            
+            nn.Conv2d(128, 3, kernel_size=1, stride=1, padding=0),
+            nn.Sigmoid(),    
+        )
+
+    def forward(self, x):
+        return F.hardtanh(self.decompress(self.attention(x) * x) + x, min_val=0.0, max_val=1.0)
+    
+    
+class DecompressNet10(nn.Module):
+    def __init__(self):
+        super(DecompressNet10, self).__init__()
+
+        self.decompress = nn.Sequential(
+            nn.Conv2d(3, 128, kernel_size=1, stride=1, padding=0),
+            nn.Dropout(0.1),
+            nn.ReLU(),
+            
+            Residual(128, 3),
+            nn.Dropout(0.1),
+            
+            Residual(128, 5),
+            nn.Dropout(0.1),
+            
+            Residual(128, 7),
+            nn.Dropout(0.1),
+            
+            Residual(128, 5),
+            nn.Dropout(0.1),
+            
+            Residual(128, 3),
+            
+            nn.Conv2d(128, 3, kernel_size=1, stride=1, padding=0),
+            nn.Hardtanh(min_val=-1.0, max_val=1.0),   
+        )
+        
+        self.attention = nn.Sequential(
+            nn.Conv2d(3, 128, kernel_size=1, stride=1, padding=0),
+            nn.Dropout(0.1),
+            nn.ReLU(),
+            
+            Residual(128, 3),
+            nn.Dropout(0.1),
+            
+            Residual(128, 5),
+            nn.Dropout(0.1),
+            
+            Residual(128, 7),
+            nn.Dropout(0.1),
+            
+            Residual(128, 5),
+            nn.Dropout(0.1),
+            
+            Residual(128, 3),
+            
+            nn.Conv2d(128, 3, kernel_size=1, stride=1, padding=0),
+            nn.Hardtanh(min_val=0.0, max_val=1.0),    
+        )
+
+    def forward(self, x):
+        return F.hardtanh(self.decompress(self.attention(x) * x) + x, min_val=0.0, max_val=1.0)
+    
+    
+class DParallelResidual(nn.Module):
+    def __init__(self, in_features):
+        super(DParallelResidual, self).__init__()
+
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(in_features, in_features, kernel_size=3, padding=1),
+            nn.ReLU(),
+            
+            nn.Conv2d(in_features, in_features, kernel_size=3, padding=1),
+        )
+            
+        self.conv7 = nn.Sequential(
+            nn.Conv2d(in_features, in_features, kernel_size=7, padding=3),
+            nn.ReLU(),
+            
+            nn.Conv2d(in_features, in_features, kernel_size=7, padding=3),
+        )
+        
+        self.conv5 = nn.Sequential(
+            nn.Conv2d(in_features, in_features, kernel_size=5, padding=2),
+            nn.ReLU(),
+            
+            nn.Conv2d(in_features, in_features, kernel_size=5, padding=2),
+        )
+        
+    def forward(self, x):
+        return torch.tanh(x + self.conv3(x) + self.conv7(x) + self.conv5(x))
+    
+# ResidualLayer(3).cuda()(torch.rand(16, 3, 128, 128).cuda()).size()
+    
+    
+class DecompressNetNA13(nn.Module):
+    def __init__(self):
+        super(DecompressNetNA13, self).__init__()
+
+        self.decompress = nn.Sequential(
+            nn.Conv2d(3, 128, kernel_size=1, stride=1, padding=0),
+            nn.ReLU(True),
+            
+            DParallelResidual(128),
+            DParallelResidual(128),
+            DParallelResidual(128),
+            DParallelResidual(128),
+            
+            nn.Conv2d(128, 3, kernel_size=1, stride=1, padding=0),
+            nn.Hardtanh(min_val=-1.0, max_val=1.0),          
+        )
+
+    def forward(self, x):
+        return F.hardtanh(self.decompress(x) + x, min_val=0.0, max_val=1.0)
+
+
+class DecompressNetNA14(nn.Module):
+    def __init__(self):
+        super(DecompressNetNA14, self).__init__()
+
+        self.decompress = nn.Sequential(
+            nn.Conv2d(3, 128, kernel_size=1, stride=1, padding=0),
+            nn.ReLU(True),
+
+            DParallelResidual(128),
+            DParallelResidual(128),
+            DParallelResidual(128),
+            DParallelResidual(128),
+            DParallelResidual(128),
+
+            nn.Conv2d(128, 3, kernel_size=1, stride=1, padding=0),
+            nn.Hardtanh(min_val=-1.0, max_val=1.0),
+        )
+
+    def forward(self, x):
+        return F.hardtanh(self.decompress(x) + x, min_val=0.0, max_val=1.0)
+
+
+class TParallelResidual(nn.Module):
+    def __init__(self, in_features):
+        super(TParallelResidual, self).__init__()
+
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(in_features, in_features, kernel_size=3, padding=1),
+            nn.ReLU(),
+            
+            nn.Conv2d(in_features, in_features, kernel_size=3, padding=1),
+        )
+            
+        self.conv7 = nn.Sequential(
+            nn.Conv2d(in_features, in_features, kernel_size=7, padding=3),
+            nn.ReLU(),
+            
+            nn.Conv2d(in_features, in_features, kernel_size=7, padding=3),
+        )
+        
+        self.conv5 = nn.Sequential(
+            nn.Conv2d(in_features, in_features, kernel_size=5, padding=2),
+            nn.ReLU(),
+            
+            nn.Conv2d(in_features, in_features, kernel_size=5, padding=2),
+        )
+        
+    def forward(self, x):
+        return torch.relu(x + self.conv3(x) + self.conv7(x) + self.conv5(x))
+    
+
+
+class SuperResolutionNetNA14(nn.Module):
+    def __init__(self):
+        super(SuperResolutionNetNA14, self).__init__()
+
+        self.decompress = nn.Sequential(
+            nn.Conv2d(3, 128, kernel_size=1, stride=1, padding=0),
+            nn.ReLU(),
+            
+            TParallelResidual(128),
+            TParallelResidual(128),
+            TParallelResidual(128),
+            
+            nn.Conv2d(128, 12, kernel_size=1, stride=1, padding=0),
+
+            nn.PixelShuffle(2),
+        )
+
+    def forward(self, x):
+        return self.decompress(x)
+
+class SuperResolutionNetNA15(nn.Module):
+    def __init__(self):
+        super(SuperResolutionNetNA15, self).__init__()
+
+        self.decompress = nn.Sequential(
+            nn.Conv2d(3, 128, kernel_size=1, stride=1, padding=0),
+            nn.ReLU(),
+            
+            TParallelResidual(128),
+            TParallelResidual(128),
+            TParallelResidual(128),
+            TParallelResidual(128),
+            
+            nn.Conv2d(128, 12, kernel_size=1, stride=1, padding=0),
+
+            nn.PixelShuffle(2),
+        )
+
+    def forward(self, x):
+        return self.decompress(x)
+
     
 def load_dn4(device="cuda", path="models/jpeg_to_raw.dn4.1569400880.model"):
     dn = DecompressNet4().to(device=device)
     dn.load_state_dict(torch.load(path, map_location=lambda storage, location: storage))
     return dn.to(device=device).eval()
 
+
+def load_dn14na(device="cuda", path="models/jpeg_to_raw.dn14na_jpeg_quiet_focused.latest.model"):
+    dn = DecompressNetNA14().to(device=device)
+    dn.load_state_dict(torch.load(path, map_location=lambda storage, location: storage))
+    return dn.to(device=device).eval()
+
+
+def load_dn10_sharpen(device="cuda", path="models/jpeg_to_raw.dn10_sharpen_quiet_focused.1571452521.model"):
+    dn = DecompressNet10().to(device=device)
+    dn.load_state_dict(torch.load(path, map_location=lambda storage, location: storage))
+    return dn.to(device=device).eval()
+
+
+def load_dn9(device="cuda", path="models/jpeg_to_raw.dn9_quiet_focused.1570854406.model"):
+    dn = DecompressNet9().to(device=device)
+    dn.load_state_dict(torch.load(path, map_location=lambda storage, location: storage))
+    return dn.to(device=device).eval()
+
+def load_dn13na(device="cuda", path="models/jpeg_to_raw.dn13na_jpeg_quiet_super_stdfocused.1573791000.model"):
+    dn = DecompressNetNA13().to(device=device)
+    dn.load_state_dict(torch.load(path, map_location=lambda storage, location: storage))
+    return dn.to(device=device).eval()
+    
+
+def load_sr14na(device="cuda", path="models/jpeg_to_raw.sr14na_jpeg.latest.model"):
+    dn = SuperResolutionNetNA14().to(device=device)
+    dn.load_state_dict(torch.load(path, map_location=lambda storage, location: storage))
+    return dn.to(device=device).eval()
+    
